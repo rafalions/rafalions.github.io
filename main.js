5f540c2f12337261fa7c801d793dc8a5c2f5d37e
rafalions <69468793+rafalions@users.noreply.github.com>
2020-08-11 20:49:00 +0800

    Update main.js

--- main.js
+++ main.js
@@ -24,22 +24,27 @@ let track_list = [
   {
     name: "Forever you",
     artist: "ZARD",
-    path: "1-10 Forever you.mp3",
+    path: "tracks/1-10 Forever you.mp3",
   },
   {
     name: "Today is another day",
     artist: "ZARD",
-    path: "3-06 Today is another day.mp3",
+    path: "tracks/3-06 Today is another day.mp3",
   },
-    {
+  {
     name: "Don't you see!",
     artist: "ZARD",
-    path: "1-01 Don't you see!.mp3",
+    path: "tracks/1-01 Don't you see!.mp3",
   },
-    {
+  {
     name: "Good-bye My Loneliness",
     artist: "ZARD",
-    path: "1-04 Good-bye My Loneliness.mp3",
+    path: "tracks/1-04 Good-bye My Loneliness.mp3",
+  },
+  {
+    name: "DAN DAN 心魅かれてく",
+    artist: "ZARD",
+    path: "tracks/13-04 DAN DAN 心魅かれてく.mp3",
   },
 ];
 
