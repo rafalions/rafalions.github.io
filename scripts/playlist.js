199b917b0bf0fd1945c6f1b240dad555c63b4a47
rafalions <69468793+rafalions@users.noreply.github.com>
2020-08-30 16:29:03 +0800

    Update playlist.js

--- scripts/playlist.js
+++ scripts/playlist.js
@@ -88,5 +88,105 @@
 		name:"負けないで",
 		artist:"ZARD",
 		src:"tracks/4-13 負けないで.mp3"
-	}
+	},
+	{
+		name:"高嶺の花子さん",
+		artist:"Back Number",
+		src:"tracks/1-01 高嶺の花子さん.mp3"
+	},
+	{
+		name:"ハッピーエンド",
+		artist:"Back Number",
+		src:"tracks/1-03 ハッピーエンド.mp3"
+	},
+	{
+		name:"クリスマスソング",
+		artist:"Back Number",
+		src:"tracks/1-04 クリスマスソング.mp3"
+	},
+	{
+		name:"ヒロイン",
+		artist:"Back Number",
+		src:"tracks/2-10 ヒロイン.mp3"
+	},
+	{
+		name:"Precious",
+		artist:"May J",
+		src:"tracks/01 Precious.mp3"
+	},
+	{
+		name:"島唄",
+		artist:"May J",
+		src:"tracks/02 島唄.mp3"
+	},
+	{
+		name:"First Love",
+		artist:"May J",
+		src:"tracks/04 First Love.mp3"
+	},
+	{
+		name:"夏祭り",
+		artist:"May J",
+		src:"tracks/06 夏祭り.mp3"
+	},
+	{
+		name:"白い雲のように",
+		artist:"May J",
+		src:"tracks/08 白い雲のように with クリス・ハート.mp3"
+	},
+	{
+		name:"secret base 〜君がくれたもの〜",
+		artist:"May J",
+		src:"tracks/09 secret base 〜君がくれたもの〜.mp3"
+	},
+	{
+		name:"ハナミズキ",
+		artist:"May J",
+		src:"tracks/11 ハナミズキ.mp3"
+	},
+	{
+		name:"I Believe [Japanese Version]",
+		artist:"May J",
+		src:"tracks/May J. - I Believe [Japanese Version] (feat. V.I from BIGBANG).mp3"
+	},
+	{
+		name:"手紙 ~拝啓 十五の君へ",
+		artist:"May J",
+		src:"tracks/May J. - Tegami ~Haikei Juugo no Kimi e~ (手紙 ~拝啓 十五の君へ~) (R. Animir).mp3"
+	},
+	{
+		name:"明日も",
+		artist:"SHISHAMO",
+		src:"tracks/11 明日も.mp3"
+	},
+	{
+		name:"Gifts",
+		artist:"Superfly",
+		src:"tracks/01. Gifts.mp3"
+	},
+	{
+		name:"Feel my soul",
+		artist:"YUI",
+		src:"tracks/YUI - Feel my soul.mp3"
+	},
+	{
+		name:"Tomorrow's Way",
+		artist:"YUI",
+		src:"tracks/YUI - Tomorrow's Way.mp3"
+	},
+	{
+		name:"Good-bye days",
+		artist:"YUI",
+		src:"tracks/YUI for 雨音薰 - Good-bye days(電影「太陽之歌」主題曲).mp3"
+	},
+	{
+		name:"It's happy line",
+		artist:"YUI",
+		src:"tracks/YUI for 雨音薰 - It's happy line 太陽之歌插曲.mp3"
+	},
+	{
+		name:"My friend",
+		artist:"YUI",
+		src:"tracks/YUI- My friend.mp3"
+	},
 ];
