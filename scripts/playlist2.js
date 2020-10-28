f5a72a19e89c7307b0a5096ae93d8f271aff56ed
rafalions <69468793+rafalions@users.noreply.github.com>
2020-10-28 20:13:33 +0800

    Create playlist2.js

---
+++ scripts/playlist2.js
@@ -0,0 +1,232 @@
+trackList = [
+	{
+		name:"Don't you see!",
+		artist:"ZARD",
+		src:"tracks/1-01 Don't you see!.mp3"
+	},
+	{
+		name:"マイ フレンド",
+		artist:"ZARD",
+		src:"tracks/1-02 マイ フレンド.mp3"
+	},
+	{
+		name:"Good-bye My Loneliness",
+		artist:"ZARD",
+		src:"tracks/1-04 Good-bye My Loneliness.mp3"
+	},
+	{
+		name:"息もできない",
+		artist:"ZARD",
+		src:"tracks/1-07 息もできない.mp3"
+	},
+	{
+		name:"Forever you",
+		artist:"ZARD",
+		src:"tracks/1-10 Forever you.mp3"
+	},
+	{
+		name:"明日を夢見て",
+		artist:"ZARD",
+		src:"tracks/1-11 明日を夢見て.mp3"
+	},
+	{
+		name:"翼を広げて",
+		artist:"ZARD",
+		src:"tracks/1-12 翼を広げて.mp3"
+	},
+	{
+		name:"心を開いて",
+		artist:"ZARD",
+		src:"tracks/2-04 心を開いて.mp3"
+	},
+	{
+		name:"揺れる想い",
+		artist:"ZARD",
+		src:"tracks/2-05 揺れる想い.mp3"
+	},
+	{
+		name:"かけがえのないもの",
+		artist:"ZARD",
+		src:"tracks/3-01 かけがえのないもの.mp3"
+	},
+	{
+		name:"DAN DAN 心魅かれてく",
+		artist:"ZARD",
+		src:"tracks/3-04 DAN DAN 心魅かれてく.mp3"
+	},
+	{
+		name:"突然",
+		artist:"ZARD",
+		src:"tracks/3-05 突然.mp3"
+	},
+	{
+		name:"Today is another day",
+		artist:"ZARD",
+		src:"tracks/3-06 Today is another day.mp3"
+	},	
+	{
+		name:"Season",
+		artist:"ZARD",
+		src:"tracks/3-07 Season.mp3"
+	},	
+	{
+		name:"永遠",
+		artist:"ZARD",
+		src:"tracks/3-10 永遠.mp3"
+	},	
+	{
+		name:"サヨナラは今もこの胸に居ます",
+		artist:"ZARD",
+		src:"tracks/3-11 サヨナラは今もこの胸に居ます.mp3"
+	},
+	{
+		name:"あの微笑みを忘れないで",
+		artist:"ZARD",
+		src:"tracks/3-13 あの微笑みを忘れないで.mp3"
+	},	
+	{
+		name:"負けないで",
+		artist:"ZARD",
+		src:"tracks/4-13 負けないで.mp3"
+	},
+	{
+		name:"高嶺の花子さん",
+		artist:"Back Number",
+		src:"tracks/1-01 高嶺の花子さん.mp3"
+	},
+	{
+		name:"ハッピーエンド",
+		artist:"Back Number",
+		src:"tracks/1-03 ハッピーエンド.mp3"
+	},
+	{
+		name:"クリスマスソング",
+		artist:"Back Number",
+		src:"tracks/1-04 クリスマスソング.mp3"
+	},
+	{
+		name:"ヒロイン",
+		artist:"Back Number",
+		src:"tracks/2-10 ヒロイン.mp3"
+	},
+	{
+		name:"Precious",
+		artist:"May J",
+		src:"tracks/01 Precious.mp3"
+	},
+	{
+		name:"島唄",
+		artist:"May J",
+		src:"tracks/02 島唄.mp3"
+	},
+	{
+		name:"First Love",
+		artist:"May J",
+		src:"tracks/04 First Love.mp3"
+	},
+	{
+		name:"夏祭り",
+		artist:"May J",
+		src:"tracks/06 夏祭り.mp3"
+	},
+	{
+		name:"白い雲のように",
+		artist:"May J",
+		src:"tracks/08 白い雲のように with クリス・ハート.mp3"
+	},
+	{
+		name:"secret base 〜君がくれたもの〜",
+		artist:"May J",
+		src:"tracks/09 secret base 〜君がくれたもの〜.mp3"
+	},
+	{
+		name:"ハナミズキ",
+		artist:"May J",
+		src:"tracks/11 ハナミズキ.mp3"
+	},
+	{
+		name:"I Believe [Japanese Version]",
+		artist:"May J",
+		src:"tracks/May J. - I Believe [Japanese Version] (feat. V.I from BIGBANG).mp3"
+	},
+	{
+		name:"手紙 ~拝啓 十五の君へ",
+		artist:"May J",
+		src:"tracks/May J. - Tegami ~Haikei Juugo no Kimi e~ (手紙 ~拝啓 十五の君へ~) (R. Animir).mp3"
+	},
+	{
+		name:"明日も",
+		artist:"SHISHAMO",
+		src:"tracks/11 明日も.mp3"
+	},
+	{
+		name:"Gifts",
+		artist:"Superfly",
+		src:"tracks/01. Gifts.mp3"
+	},
+	{
+		name:"Feel my soul",
+		artist:"YUI",
+		src:"tracks/YUI - Feel my soul.mp3"
+	},
+	{
+		name:"Tomorrow's Way",
+		artist:"YUI",
+		src:"tracks/YUI - Tomorrow's Way.mp3"
+	},
+	{
+		name:"Good-bye days",
+		artist:"YUI",
+		src:"tracks/YUI for 雨音薰 - Good-bye days(電影「太陽之歌」主題曲).mp3"
+	},
+	{
+		name:"It's happy line",
+		artist:"YUI",
+		src:"tracks/YUI for 雨音薰 - It's happy line 太陽之歌插曲.mp3"
+	},
+	{
+		name:"My friend",
+		artist:"YUI",
+		src:"tracks/YUI- My friend.mp3"
+	},
+	{
+		name:"always",
+		artist:"倉木麻衣",
+		src:"tracks/always.mp3"
+	},
+	{
+		name:"Growing of my heart",
+		artist:"倉木麻衣",
+		src:"tracks/Growing of my heart.mp3"
+	},
+	{
+		name:"Secret of my heart",
+		artist:"倉木麻衣",
+		src:"tracks/Secret of my heart.mp3"
+	},
+	{
+		name:"Start in my life",
+		artist:"倉木麻衣",
+		src:"tracks/Start in my life.mp3"
+	},
+	{
+		name:"Time after time",
+		artist:"倉木麻衣",
+		src:"tracks/Time after time~花舞う街で~.mp3"
+	},
+	{
+		name:"風のららら",
+		artist:"倉木麻衣",
+		src:"tracks/風のららら.mp3"
+	},
+	{
+		name:"Winter Bells",
+		artist:"倉木麻衣",
+		src:"tracks/Winter Bells.mp3"
+	},
+	{
+		name:"渡月橋 ~君 想ふ~",
+		artist:"倉木麻衣",
+		src:"tracks/渡月橋 ~君 想ふ~.mp3"
+	}
+];
